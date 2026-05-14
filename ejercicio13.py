7054f8d1dfdb989051046dee6200bf601a633057
ericagramuglia-max <ericagramuglia@gmail.com>
2026-05-14 14:05:15 -0300

    ejercicio con procedimiento

---
+++ ejercicio13.py
@@ -0,0 +1,9 @@
+"""PROCEDIMIENTOS
+13. Mostrar un mensaje de bienvenida
+Situación: Crear un procedimiento que muestre un mensaje de bienvenida al programa.
+"""
+
+def saludar():
+    print("Bienvenidos")
+
+saludar();
